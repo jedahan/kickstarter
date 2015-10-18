3282ed5321f5d7fc310c3942d0ae861b3f88c7b3
Jonathan Dahan <jonathan@jonathan.is>
2015-10-18 16:40:09 -0400

    Add argument parsing for project and back

--- mini-ksr.py
+++ mini-ksr.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
+import sys
 
-print('Added Awesome_Sauce project with target of $500')
+command = sys.argv[0].split('/')[-1]
+
+if command == 'ksr':
+  sys.argv.pop(0)
+  command = sys.argv[0].split('/')[-1]
+
+if command == "project":
+  project = sys.argv[1]
+  target = int(sys.argv[2])
+  print("Added %s project with target of $%i" % (project, target) )
+elif command == "back":
+  person = sys.argv[1]
+  project = sys.argv[2]
+  card = sys.argv[3]
+  amount = sys.argv[4]
+  print("%s backed project %s $%i" % (person, project, amount) )
