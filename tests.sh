d06fbdd86e2178b28b89bd87303fec311bcbc2e6
Jonathan Dahan <jonathan@jonathan.is>
2015-10-19 15:07:04 -0400

    Add newlines manually to tests so they pass

--- tests.sh
+++ tests.sh
@@ -21,21 +21,21 @@ assert 'ksr back Jane Awesome_Sauce 4111111111111111 50' \
 'ERROR: That card has already been added by another user!'
 
 assert 'ksr back Jane Awesome_Sauce 5555555555554444 50' \
-''
+'Jane backed project Awesome_Sauce for $50'
 
 assert 'ksr list Awesome_Sauce' \
-'-- John backed for $50
--- Jane backed for $50
-Awesome_Sauce needs $400 more dollars to be successful'
+'-- John backed for $50\n'\
+'-- Jane backed for $50\n'\
+'Awesome_Sauce needs $400 more dollars to be successful'
 
 assert 'ksr back Mary Awesome_Sauce 5474942730093167 400' \
 'Mary backed project Awesome_Sauce for $400'
 
 assert 'ksr list Awesome_Sauce' \
-'-- John backed for $50
--- Jane backed for $50
--- Mary backed for $400
-Awesome_Sauce is successful!'
+'-- John backed for $50\n'\
+'-- Jane backed for $50\n'\
+'-- Mary backed for $400\n'\
+'Awesome_Sauce is successful!'
 
 assert 'ksr backer John' \
 '-- Backed Awesome_Sauce for $50'
