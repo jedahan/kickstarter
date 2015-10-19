0deffe47d042f15086448f4c478ac431234f3d80
Jonathan Dahan <jonathan@jonathan.is>
2015-10-19 12:30:03 -0400

    First pass at using click for argument parsing

--- mini-ksr.py
+++ mini-ksr.py
@@ -1,19 +1,63 @@
 #!/usr/bin/env python3
+import click
 import sys
 
-command = sys.argv[0].split('/')[-1]
+backings = set()
 
-if command == 'ksr':
-  sys.argv.pop(0)
-  command = sys.argv[0].split('/')[-1]
+@click.group()
+def cli():
+  """ksr is a command line tool to manage crowdfunding campaigns"""
+  global projects
+  projects = dict()
+  pass
 
-if command == "project":
-  project = sys.argv[1]
-  target = int(sys.argv[2])
-  print("Added %s project with target of $%i" % (project, target) )
-elif command == "back":
-  person = sys.argv[1]
-  project = sys.argv[2]
-  card = sys.argv[3]
-  amount = sys.argv[4]
+def error(string):
+  print(string)
+  sys.exit(1)
+
+@cli.command()
+@click.argument('name')
+@click.argument('target', type=int)
+def project(name, target):
+  """Create a new project"""
+  if name in projects:
+    error("Project %s already exists, please choose a unique name" % name)
+  else:
+    projects[name] = target
+    print("Added %s project with target of $%i" % (name, target) )
+
+@cli.command('list')
+@click.argument('project', required=False)
+def list_project(project):
+  """List project details, or all projects if no name is specified"""
+  if project not in projects:
+    if len(projects) > 0:
+      print("Projects:\n-- "+"\n-- ".join(projects))
+  else:
+    amount = 0
+    for backing in backings:
+      amount = amount + backing.amount
+      print("-- %s backed for $%i" % (backing.name, backing.amount) )
+    if amount > projects[project]:
+      print("%s is successfull!")
+    else:
+      print("%s needs %i more dollars to be successful" % projects[name] - amount)
+
+@cli.command('back')
+@click.argument('person')
+@click.argument('project')
+@click.argument('credit_card', type=int)
+@click.argument('amount', type=int)
+def back(person, project, credit_card, amount):
+  """Back a project"""
+  if project not in projects:
+    error("Project %s does not exist" % project)
+  backings.add({
+    person: person,
+    project: project,
+    credit_card: credit_card,
+    amount: amount})
   print("%s backed project %s $%i" % (person, project, amount) )
+
+if __name__ == "__main__":
+    cli()
