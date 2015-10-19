6340abc9c09a11045026d6b084735b8f4e0b8d0e
Jonathan Dahan <jonathan@jonathan.is>
2015-10-19 13:23:54 -0400

    Start persisting projects and backings with tinydb

--- mini-ksr.py
+++ mini-ksr.py
@@ -1,63 +1,72 @@
 #!/usr/bin/env python3
 import click
 import sys
+from tinydb import TinyDB, where
 
-backings = set()
+db = TinyDB('db.json')
+projects = db.table('projects')
+backings = db.table('backings')
 
 @click.group()
 def cli():
   """ksr is a command line tool to manage crowdfunding campaigns"""
-  global projects
-  projects = dict()
   pass
 
 def error(string):
   print(string)
   sys.exit(1)
 
+def get_project(name):
+  return projects.get(where('name') == name) or error("Project %r not found" % name)
+
 @cli.command()
 @click.argument('name')
 @click.argument('target', type=int)
 def project(name, target):
   """Create a new project"""
-  if name in projects:
+  if projects.search(where('name') == name):
     error("Project %s already exists, please choose a unique name" % name)
   else:
-    projects[name] = target
+    projects.insert({'name': name, 'target': target})
     print("Added %s project with target of $%i" % (name, target) )
 
 @cli.command('list')
-@click.argument('project', required=False)
-def list_project(project):
+@click.argument('name', required=False)
+def list_project(name):
   """List project details, or all projects if no name is specified"""
-  if project not in projects:
-    if len(projects) > 0:
-      print("Projects:\n-- "+"\n-- ".join(projects))
+  if name == None:
+    all_projects = projects.all()
+    if len(all_projects):
+      print("Projects:\n-- " + "\n-- ".join([p['name'] for p in all_projects]))
   else:
+    project = get_project(name)
     amount = 0
-    for backing in backings:
-      amount = amount + backing.amount
-      print("-- %s backed for $%i" % (backing.name, backing.amount) )
-    if amount > projects[project]:
+    for backing in backings.all():
+      amount = amount + backing['amount']
+      print("-- %s backed for $%i" % (backing['person'], backing['amount']) )
+    if amount > project['target']:
       print("%s is successfull!")
     else:
-      print("%s needs %i more dollars to be successful" % projects[name] - amount)
+      print("%s needs %i more dollars to be successful" % (name, project['target'] - amount))
 
 @cli.command('back')
 @click.argument('person')
 @click.argument('project')
 @click.argument('credit_card', type=int)
-@click.argument('amount', type=int)
+@click.argument('amount', type=float)
 def back(person, project, credit_card, amount):
   """Back a project"""
-  if project not in projects:
-    error("Project %s does not exist" % project)
-  backings.add({
-    person: person,
-    project: project,
-    credit_card: credit_card,
-    amount: amount})
-  print("%s backed project %s $%i" % (person, project, amount) )
+  project = get_project(project)
+  backing = {
+    'person': person,
+    'project': project,
+    'credit_card': credit_card,
+    'amount': amount}
+  if backings.contains((where('person')==person) & (where('project')==project)):
+    error("%r has already backed %r" % (person, project))
+  else:
+    backings.insert(backing)
+    print("%s backed project %s for $%i" % (person, project['name'], amount) )
 
 if __name__ == "__main__":
     cli()
--- tests.sh
+++ tests.sh
@@ -6,6 +6,8 @@ set -e
 
 export PATH="bin:$PATH"
 
+rm db.json
+
 assert 'ksr project Awesome_Sauce 500' \
 'Added Awesome_Sauce project with target of $500'
 
@@ -22,18 +24,18 @@ assert 'ksr back Jane Awesome_Sauce 5555555555554444 50' \
 ''
 
 assert 'ksr list Awesome_Sauce' \
-'-- John backed for $50' \
-'-- Jane backed for $50' \
-'Awesome_Sauce needs $400 more dollars to be successful'
+'-- John backed for $50
+-- Jane backed for $50
+Awesome_Sauce needs $400 more dollars to be successful'
 
 assert 'ksr back Mary Awesome_Sauce 5474942730093167 400' \
 'Mary backed project Awesome_Sauce for $400'
 
 assert 'ksr list Awesome_Sauce' \
-'-- John backed for $50' \
-'-- Jane backed for $50' \
-'-- Mary backed for $400' \
-'Awesome_Sauce is successful!'
+'-- John backed for $50
+-- Jane backed for $50
+-- Mary backed for $400
+Awesome_Sauce is successful!'
 
 assert 'ksr backer John' \
 '-- Backed Awesome_Sauce for $50'
