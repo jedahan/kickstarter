d75e7b7a7636d7df926ff135551b70b32193eaa6
Jonathan Dahan <jonathan@jonathan.is>
2015-10-20 16:20:30 -0400

    Revert "Cleanup assertions, fixes #17"
    This reverts commit 88a15ed02662191aabcc32582cdadcbb2d8302a2.

--- tests.sh
+++ tests.sh
@@ -21,17 +21,19 @@ assert './ksr.py back Jane Awesome_Sauce 4111111111111111 50' \
 assert './ksr.py back Jane Awesome_Sauce 5555555555554444 50' \
 'Jane backed project Awesome_Sauce for $50'
 
-assert './ksr.py list Awesome_Sauce' '-- John backed for $50
--- Jane backed for $50
-Awesome_Sauce needs $400 more dollars to be successful'
+assert './ksr.py list Awesome_Sauce' \
+'-- John backed for $50\n'\
+'-- Jane backed for $50\n'\
+'Awesome_Sauce needs $400 more dollars to be successful'
 
 assert './ksr.py back Mary Awesome_Sauce 5474942730093167 400' \
 'Mary backed project Awesome_Sauce for $400'
 
-assert './ksr.py list Awesome_Sauce' '-- John backed for $50
--- Jane backed for $50
--- Mary backed for $400
-Awesome_Sauce is successful!'
+assert './ksr.py list Awesome_Sauce' \
+'-- John backed for $50\n'\
+'-- Jane backed for $50\n'\
+'-- Mary backed for $400\n'\
+'Awesome_Sauce is successful!'
 
 assert './ksr.py backer John' \
 '-- Backed Awesome_Sauce for $50'
@@ -42,8 +44,9 @@ assert './ksr.py project Floating_target 500.01' \
 assert './ksr.py back Kwabena Floating_target 4563960122001999 50.01' \
 'Kwabena backed project Floating_target for $50.01'
 
-assert './ksr.py list Floating_target' '-- Kwabena backed for $50.01
-Floating_target needs $450 more dollars to be successful'
+assert './ksr.py list Floating_target' \
+'-- Kwabena backed for $50.01\n'\
+'Floating_target needs $450 more dollars to be successful'
 
 assert './ksr.py project Add_then_remove 500' \
 'Added Add_then_remove project with target of $500'
@@ -60,11 +63,13 @@ assert './ksr.py list Add_then_remove' \
 assert './ksr.py back Mary Add_then_remove 4222222222220 99.99' \
 'Mary backed project Add_then_remove for $99.99'
 
-assert './ksr.py list Add_then_remove' '-- Mary backed for $99.99
-Add_then_remove needs $400.01 more dollars to be successful'
+assert './ksr.py list Add_then_remove' \
+'-- Mary backed for $99.99\n'\
+'Add_then_remove needs $400.01 more dollars to be successful'
 
-assert './ksr.py backer Mary' '-- Backed Awesome_Sauce for $400
--- Backed Add_then_remove for $99.99'
+assert './ksr.py backer Mary' \
+'-- Backed Awesome_Sauce for $400\n'\
+'-- Backed Add_then_remove for $99.99'
 
 # Should fail for non alphanumeric, or short or long strings
 # click failures exit with error code 2
